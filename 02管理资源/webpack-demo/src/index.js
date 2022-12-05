cfab01388da4d0abb8b980c1d4ad5b54185a1ba4
xzh <1322739583@qq.com>
2022-12-05 19:38:31 +0800

    管理资源

--- 02管理资源/webpack-demo/src/index.js
+++ 02管理资源/webpack-demo/src/index.js
@@ -1,14 +1,25 @@
 import _ from 'lodash';
-//1.添加代码一
 import './style.css';
+//1.添加代码一
+import Icon from './webpack.svg';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    //2.添加代码二
+    element.innerHTML = _.join(['hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    //2.添加代码二
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data)
+    console.log(Notes)
 
     return element;
 }
